be38eaef6ec154704ead76954c3f8032425dae88
noname1288 <lequang070203@gmail.com>
2025-05-15 01:54:30 +0700

    first step to deploy on render

--- services/gateway/src/main/java/com/soa/gateway/config/WebClientConfig.java
+++ services/gateway/src/main/java/com/soa/gateway/config/WebClientConfig.java
@@ -9,15 +9,21 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.beans.factory.annotation.Value;
+
 
 import java.util.List;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${app.services.user-service}")
+    private String userServiceUrl;
+
     @Bean
     WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8081/user")
+                .baseUrl(userServiceUrl)
                 .build();
     }
 
